4f0b68f09f60f10e1face7b8492aa3fd9747125c
Eric Marques <eric.marquesgomesmarques@gmail.com>
2023-11-07 18:43:12 -0300

    Outro exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Arrayzada from "./Aulas/Arraysnojsx";
 import Exercicioum from "./Aulas/Exercicioum";
+import ListaProds from "./Aulas/Exerciciodois";
 
 function App() {
   return (
     <>
-    <Exercicioum/>
-    <Arrayzada/>
+    {/* <Exercicioum/>
+    <Arrayzada/> */}
+    <ListaProds/>
     </>
   )
 }
--- src/Aulas/Arraysnojsx.jsx
+++ src/Aulas/Arraysnojsx.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 // nunca iremos criar um array assim, isso é apenas para aprendizado (produtos) no caso.
+// coisa horivel quando a pessoa bate o olho lol
 function arrayzada() {
     const produtos = [<li key="e1">PC</li>, <li key="e2">Smartphone</li>, <li key="e3">Ipad</li>]
     const filmes = ['Before', 'The', 'Sunrise']
+    const livros = [
+        { nome: "game of thrones", ano: 1996},
+        { nome: "Storm of swords", ano: 2000},
+        { nome: "Clash royale hehe haha", ano: 2017},
+    ]
     return (
         <>
-            {filmes.map(filme => (
-                <li key={filme}>{filme}</li>
+        <ul>
+            {livros.map(({nome, ano}) => (
+                <li key={nome}>
+                    {nome}, {ano}
+                </li>
             ))}
+        </ul>
         </>
     )
 }
